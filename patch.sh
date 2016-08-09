3b8ba18da4a60424498d51ccc90c0ca067dbff3a
Steven Lai <aqunxgm@gmail.com>
2016-08-09 22:36:13 +0800

    add patch.sh file

---
+++ patch.sh
@@ -0,0 +1,4 @@
+sudo apt-get --assume-yes update        # Fetches the list of available updates
+sudo apt-get --assume-yes upgrade       # Strictly upgrades the current packages
+sudo apt-get --assume-yes dist-upgrade  # Installs updates (new ones)
+
